9aaf04c8dcad7bd4f9f5a8aa7008fbb144ed121c
Ismael CHARI <charibossollati.ismael@gmail.com>
2026-08-09 22:26:39 +0200

    Bump download links to v3.0.14.

--- src/config.js
+++ src/config.js
@@ -15,7 +15,7 @@ export const site = {
 }
 
 export const downloadLinks = {
-  apk: 'https://github.com/chari-ismael/arich-player/releases/download/v3.0.13/ArichPlayer-v3.0.13.apk',
+  apk: 'https://github.com/chari-ismael/arich-player/releases/download/v3.0.14/ArichPlayer-v3.0.14.apk',
   googlePlay: 'https://play.google.com/store/apps/details?id=com.arich.iptv',
   qrTarget: 'https://github.com/chari-ismael/arich-player/releases/latest',
 }
